d00da3ff99dbe35d4a608037b15cf5f0e9d91f28
Trey Shelton <tshelton@basecampcodingacademy.org>
2017-09-12 10:46:07 -0500

    nate made a new version of the 'run' function. now updated

--- tui.py
+++ tui.py
@@ -1,58 +1,5 @@
-import curses, driver, time
-
-
-def run(init, update, view, rate=None):
-    '''(State, (String, State) -> State, (State, Int, Int) -> String, Int) -> None
-
-    Helper inspired by The Elm Architecture for running simple terminal applications.
-    `init` is the initial state of the application.
-    `update` is a transition function from the current state
-        to the next state when given an input character.
-    `view` returns the string that should be printed for the current state of the application
-        given the width and height of the curses window.
-    `rate` is the number of times per second 'TICK` will be provided to update. By default,
-        `rate` is none, and the application will block on input. If `rate` is provided,
-        the application will not block on input.
-
-    For example, a simple counter application might look like:
-
-    def update(key, state):
-        return state + 1
-    
-    def view(state, width, height):
-        return 'The current number is: {}'.format(state)
-
-    if __name__ == '__main__':
-        run(0, update, view)
-    '''
-
-    def helper(stdscr):
-        state = init
-        y, x = stdscr.getmaxyx()
-        stdscr.addstr(0, 0, view(state, x, y))
-        if not (rate is None):
-            stdscr.nodelay(1)
-            wait = 1 / rate
-            previous_tick = time.time()
-        while True:
-            if not (rate is None) and time.time() - previous_tick > wait:
-                previous_tick = time.time()
-                state = update('TICK', state)
-                y, x = stdscr.getmaxyx()
-                stdscr.addstr(0, 0, view(state, x, y))
-            try:
-                key = stdscr.getkey()
-            except KeyboardInterrupt:
-                return
-            except:
-                pass
-            else:
-                stdscr.clear()
-                state = update(key, state)
-                y, x = stdscr.getmaxyx()
-                stdscr.addstr(0, 0, view(state, x, y))
-
-    curses.wrapper(helper)
+import driver, time
+from pybcca.tui_helper import run
 
 
 def key_to_action(key):
@@ -90,13 +37,13 @@ def key_to_action(key):
 
 
 def update(key, state):
-    state = state.game_update(key_to_action(key))
-    if state.keep_going():
-        return state
-    else:
-        print('GAME OVER')
-        time.sleep(3)
-        exit()
+    return state.game_update(key_to_action(key))
+
+
+def final_view(state, w, h):
+    return '''Game Over:
+Score: {}
+'''.format(state.score)
 
 
 def view(state, width, height):
@@ -104,7 +51,12 @@ def view(state, width, height):
 
 
 def main():
-    run(driver.Driver(50, 20, 25), update, view, 15)
+    run(driver.Driver(50, 20, 25),
+        update,
+        view,
+        15,
+        quit_when=lambda s: not s.keep_going(),
+        final_view=final_view)
 
 
 if __name__ == '__main__':
